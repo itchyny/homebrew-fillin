540f04e8399ea1ca4dcdaa20b55d3ec6c2af8faa
itchyny <itchyny@hatena.ne.jp>
2017-06-14 06:05:38 +0900

    fillin v0.0.5

--- fillin.rb
+++ fillin.rb
@@ -1,13 +1,13 @@
 class Fillin < Formula
   homepage 'https://github.com/itchyny/fillin'
-  version '0.0.4'
+  version '0.0.5'
 
   if Hardware::CPU.is_64_bit?
-    url 'https://github.com/itchyny/fillin/releases/download/v0.0.4/fillin_darwin_amd64.zip'
-    sha256 'db498710980ec6c438b839ecca621a7086347522ea163800a463ef4635ece2ca'
+    url 'https://github.com/itchyny/fillin/releases/download/v0.0.5/fillin_darwin_amd64.zip'
+    sha256 '6c61a36b3570de517f96d930f92c64beb193ff005ad82dd39df5c8a44d2e2678'
   else
-    url 'https://github.com/itchyny/fillin/releases/download/v0.0.4/fillin_darwin_386.zip'
-    sha256 'e186f618f8f26cc112514f35ede0de4c26034dc01b881030dbfd5b7777bd1419'
+    url 'https://github.com/itchyny/fillin/releases/download/v0.0.5/fillin_darwin_386.zip'
+    sha256 '4975ceae3e6dd4a7191c542c459393409377126d1f4a0450bf633a25f0100ec9'
   end
 
   head do
